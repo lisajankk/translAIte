5911f1bc0c4335d456d3e47b101e0b3f63e1c2d4
paragon <pol.aragones@epitech.eu>
2024-03-09 18:45:51 +0100

    [ADD]/Bookmark system to save translations in bookmarks

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     var translatedTextArea = document.getElementById("translatedTextArea");
     var textToTranslateArea = document.getElementById("textToTranslateArea");
     var howto = document.getElementById("howto");
+    var downloadTranslate = document.getElementById("downloadTranslate");
+    var copyTranslate = document.getElementById("copyTranslate");
+    var bookmark_container = document.getElementById("bookmark-container");
+    var bookmarkTranslate = document.getElementById("bookmarkTranslate");
 
     var to_spanish = document.getElementById("to_spanish");
     var to_english = document.getElementById("to_english");
@@ -16,7 +20,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var originLanguage = "spanish";
     var transLanguage = "english";
-    const token = "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";
+    var bookmarks = [];
+    var index = 0;
+    const token = "";
 
     window.onclick = function(event) {
         if (!event.target.matches('.languageSection .translateToLanguage')) {
@@ -61,4 +67,58 @@ document.addEventListener("DOMContentLoaded", function() {
     from_spanish.addEventListener("click", function() {originLanguage = "spanish"; from_lang.textContent = originLanguage});
     from_english.addEventListener("click", function() {originLanguage = "english"; from_lang.textContent = originLanguage});
     from_swedish.addEventListener("click", function() {originLanguage = "swedish"; from_lang.textContent = originLanguage});
+
+    downloadTranslate.addEventListener("click", function() {
+        var blob = new Blob([translatedTextArea.value], { type: 'text/plain' });
+        var a = document.createElement('a');
+
+        a.href = window.URL.createObjectURL(blob);
+        a.download = 'translation.txt';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    });
+
+    copyTranslate.addEventListener("click", function() {
+        translatedTextArea.select();
+        document.execCommand('copy');
+        translatedTextArea.setSelectionRange(0, 0);
+        alert('Text copied to clipboard!');
+    });
+
+    bookmarkTranslate.addEventListener("click", function() {
+            var p = document.createElement("p");
+            p.id = index;
+            index++;
+            p.style.backgroundColor = "white";
+            p.style.color = "black";
+            p.style.padding = "1em"
+            p.style.borderRadius = "5px";
+            p.style.width = "20%";
+            p.style.cursor = "pointer";
+            var now = new Date();
+            var day = now.getDate();
+            var month = now.getMonth() + 1;
+            var hour = now.getHours();
+            var minute = now.getMinutes();
+            
+            day = (day < 10) ? '0' + day : day;
+            month = (month < 10) ? '0' + month : month;
+            hour = (hour < 10) ? '0' + hour : hour;
+            minute = (minute < 10) ? '0' + minute : minute;
+            
+            var displayDate = day + '/' + month + ' ' + hour + ':' + minute + '     |     ';
+            
+            p.textContent = displayDate + textToTranslateArea.value.substring(0, 10) + '...';
+            bookmark_container.appendChild(p);
+            
+            bookmarks.push({'date': displayDate, 'from': textToTranslateArea.value, 'to': translatedTextArea.value});
+
+            p.addEventListener("click", function() {
+                var i = this.id;
+                textToTranslateArea.value = bookmarks[i].from;
+                translatedTextArea.value = bookmarks[i].to;
+            });
+    });
+
 });
