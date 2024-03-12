89b90f64b6bef08a1e53e52831455976f9ea2d2c
paragon <pol.aragones@epitech.eu>
2024-03-12 23:54:04 +0100

    [ADD]/token with input, minor changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var translate = document.getElementById("translate");
     var translatedTextArea = document.getElementById("translatedTextArea");
     var textToTranslateArea = document.getElementById("textToTranslateArea");
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var bookmark_container = document.getElementById("bookmark-container");
     var bookmarkTranslate = document.getElementById("bookmarkTranslate");
     var speak = document.getElementById("text-to-speech");
+    var read = document.getElementById('file-upload');
+    var token_inp = document.getElementById("token_input");
 
     var to_spanish = document.getElementById("to_spanish");
     var to_english = document.getElementById("to_english");
@@ -26,9 +28,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var transLanguage = "english";
     var bookmarks = [];
     var index = 0;
-    const token = "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";
 
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (!event.target.matches('.languageSection .translateToLanguage')) {
             var dropdowns = document.getElementsByClassName("dropdown-content");
             var i;
@@ -41,7 +42,8 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    speak.addEventListener("click", function() {
+    speak.addEventListener("click", function () {
+        var token = token_inp.value;
         if (translatedTextArea.value) {
             fetch("https://api.openai.com/v1/audio/speech", {
                 method: 'POST',
@@ -55,19 +57,21 @@ document.addEventListener("DOMContentLoaded", function() {
                     "voice": "onyx",
                 })
             }).then(response => response.blob())
-            .then(blob => {
-                var blobUrl = URL.createObjectURL(blob);
-                var audio = new Audio(blobUrl);
-                audio.play();
-            })
-            .catch(error => {
-                console.error('Error fetching audio:', error);
-            });
+                .then(blob => {
+                    var blobUrl = URL.createObjectURL(blob);
+                    var audio = new Audio(blobUrl);
+                    audio.play();
+                })
+                .catch(error => {
+                    console.error('Error fetching audio:', error);
+                });
         }
     });
 
-    translate.addEventListener("click", function() {
+    translate.addEventListener("click", function () {
         var text = textToTranslateArea.value;
+        var token = token_inp.value;
+
         if (to_other.value) {
             transLanguage = to_other.value;
         }
@@ -85,26 +89,27 @@ document.addEventListener("DOMContentLoaded", function() {
             },
             body: JSON.stringify({
                 "model": "gpt-3.5-turbo",
-                "messages": [{"role": "user", "content": `translate this text in ${originLanguage}: "${text}" to ${transLanguage}. Do not include anything else in your reply that is not the translated text ${howto.value}`}]
+                "messages": [{ "role": "user", "content": `translate this text in ${originLanguage}: "${text}" to ${transLanguage}. Do not include anything else in your reply that is not the translated text ${howto.value}` }]
             })
         }).then(response => {
             return response.json();
         }).then(data => {
-            console.log(data);
             translatedTextArea.value = data.choices[0].message.content;
+        }).catch(error => {
+            alert("please enter a valid api token")
         })
     });
 
-    to_spanish.addEventListener("click", function() {transLanguage = "spanish"; to_lang.textContent = transLanguage});
-    to_english.addEventListener("click", function() {transLanguage = "english"; to_lang.textContent = transLanguage});
-    to_swedish.addEventListener("click", function() {transLanguage = "swedish"; to_lang.textContent = transLanguage});
+    to_spanish.addEventListener("click", function () { transLanguage = "spanish"; to_lang.textContent = transLanguage });
+    to_english.addEventListener("click", function () { transLanguage = "english"; to_lang.textContent = transLanguage });
+    to_swedish.addEventListener("click", function () { transLanguage = "swedish"; to_lang.textContent = transLanguage });
 
-    from_spanish.addEventListener("click", function() {originLanguage = "spanish"; from_lang.textContent = originLanguage});
-    from_english.addEventListener("click", function() {originLanguage = "english"; from_lang.textContent = originLanguage});
-    from_swedish.addEventListener("click", function() {originLanguage = "swedish"; from_lang.textContent = originLanguage});
-    from_detect.addEventListener("click", function() {originLanguage = "detect"; from_lang.textContent = originLanguage});
+    from_spanish.addEventListener("click", function () { originLanguage = "spanish"; from_lang.textContent = originLanguage });
+    from_english.addEventListener("click", function () { originLanguage = "english"; from_lang.textContent = originLanguage });
+    from_swedish.addEventListener("click", function () { originLanguage = "swedish"; from_lang.textContent = originLanguage });
+    from_detect.addEventListener("click", function () { originLanguage = "detect"; from_lang.textContent = originLanguage });
 
-    downloadTranslate.addEventListener("click", function() {
+    downloadTranslate.addEventListener("click", function () {
         var blob = new Blob([translatedTextArea.value], { type: 'text/plain' });
         var a = document.createElement('a');
 
@@ -115,46 +120,56 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.removeChild(a);
     });
 
-    copyTranslate.addEventListener("click", function() {
+    copyTranslate.addEventListener("click", function () {
         translatedTextArea.select();
         document.execCommand('copy');
         translatedTextArea.setSelectionRange(0, 0);
         alert('Text copied to clipboard!');
     });
 
-    bookmarkTranslate.addEventListener("click", function() {
-            var p = document.createElement("p");
-            p.id = index;
-            index++;
-            p.style.backgroundColor = "white";
-            p.style.color = "black";
-            p.style.padding = "1em"
-            p.style.borderRadius = "5px";
-            p.style.width = "20%";
-            p.style.cursor = "pointer";
-            var now = new Date();
-            var day = now.getDate();
-            var month = now.getMonth() + 1;
-            var hour = now.getHours();
-            var minute = now.getMinutes();
-            
-            day = (day < 10) ? '0' + day : day;
-            month = (month < 10) ? '0' + month : month;
-            hour = (hour < 10) ? '0' + hour : hour;
-            minute = (minute < 10) ? '0' + minute : minute;
-            
-            var displayDate = day + '/' + month + ' ' + hour + ':' + minute + '     |     ';
-            
-            p.textContent = displayDate + textToTranslateArea.value.substring(0, 10) + '...';
-            bookmark_container.appendChild(p);
-            
-            bookmarks.push({'date': displayDate, 'from': textToTranslateArea.value, 'to': translatedTextArea.value});
+    bookmarkTranslate.addEventListener("click", function () {
+        var p = document.createElement("p");
+        p.id = index;
+        index++;
+        p.style.backgroundColor = "white";
+        p.style.color = "black";
+        p.style.padding = "1em"
+        p.style.borderRadius = "5px";
+        p.style.width = "20%";
+        p.style.cursor = "pointer";
+        var now = new Date();
+        var day = now.getDate();
+        var month = now.getMonth() + 1;
+        var hour = now.getHours();
+        var minute = now.getMinutes();
 
-            p.addEventListener("click", function() {
-                var i = this.id;
-                textToTranslateArea.value = bookmarks[i].from;
-                translatedTextArea.value = bookmarks[i].to;
-            });
+        day = (day < 10) ? '0' + day : day;
+        month = (month < 10) ? '0' + month : month;
+        hour = (hour < 10) ? '0' + hour : hour;
+        minute = (minute < 10) ? '0' + minute : minute;
+
+        var displayDate = day + '/' + month + ' ' + hour + ':' + minute + '     |     ';
+
+        p.textContent = displayDate + textToTranslateArea.value.substring(0, 10) + '...';
+        bookmark_container.appendChild(p);
+
+        bookmarks.push({ 'date': displayDate, 'from': textToTranslateArea.value, 'to': translatedTextArea.value });
+
+        p.addEventListener("click", function () {
+            var i = this.id;
+            textToTranslateArea.value = bookmarks[i].from;
+            translatedTextArea.value = bookmarks[i].to;
+        });
+    });
+
+    read.addEventListener('change', function(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var content = e.target.result;
+            textToTranslateArea.value = content;
+        };
+        reader.readAsText(file);
     });
 
 });
